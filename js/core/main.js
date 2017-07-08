ebc2b31669a85bb583492c8405a6dc05393beefd
Darkfafi <ik_ben_ramses@hotmail.com>
2017-07-08 10:00:35 +0200

    Created the Scene and SceneManager classes and tested them in the main scene

--- js/core/main.js
+++ js/core/main.js
@@ -1,3 +1,4 @@
+var sceneManager;
 var app;
 
 window.onload = function()
@@ -9,7 +10,67 @@ function main()
 {
 	app = new PIXI.Application(800, 600, {backgroundColor : 0x2299ff});
 	document.getElementById("wrapper").appendChild(app.view);
+
+	sceneManager = new SceneManager(app);
 	
-	var testGrid = new Grid(10, 10);
-	console.log(testGrid.getAllTiles());
+	app.ticker.add(function()
+	{
+		sceneManager.update(app.ticker.elapsedMS/1000);
+	});
+
+	sceneManager.addScene("testScene", TestScene);
+	sceneManager.addScene("testScene2", TestScene2);
+	sceneManager.setScene("testScene");
+}
+
+TestScene.prototype = Object.create(Scene.prototype);
+function TestScene()
+{
+	Scene.call(this);
+	var t = 0;
+	var c = 0;
+	this.onCreate = function()
+	{
+		console.log("hello scene");
+	}
+
+	this.onUpdate = function(deltaTime)
+	{
+		t += deltaTime;
+		if(t > 1)
+		{
+			t = 0;
+			c++;
+			console.log("second ticker: " + c);
+			if(c == 5)
+			{
+				sceneManager.setScene("testScene2")
+			}
+		}
+	}
+
+	this.onDestroy = function()
+	{
+		console.log("bye scene");
+	}
+}
+
+TestScene2.prototype = Object.create(Scene.prototype);
+function TestScene2()
+{
+	Scene.call(this);
+	this.onCreate = function()
+	{
+		console.log("hello scene2");
+	}
+
+	this.onUpdate = function(deltaTime)
+	{
+		
+	}
+
+	this.onDestroy = function()
+	{
+
+	}
 }
---
+++ js/core/scenes/sceneManager.js
@@ -0,0 +1,88 @@
+
+function SceneManager(pixiApplication)
+{
+	this.currentScene = null;
+	this.scenes = [];
+	
+	var pixiApplication = pixiApplication;
+	var sceneSystem = this;
+
+	this.getPixiApplication = function()
+	{
+		return pixiApplication;
+	}
+}
+
+SceneManager.prototype.addScene = function(sceneName, scenePrototype)
+{
+	var sp = this.getSceneIndex(sceneName);
+	if(sp != -1) { alert("Can't add scene with name: " + sceneName  +". There is already a scene with that name."); return; }
+	this.scenes.push(new SceneManagerItem(sceneName, scenePrototype));
+}
+
+SceneManager.prototype.removeScene = function(sceneName)
+{
+	var sp = this.getSceneIndex(sceneName);
+	if(sp == -1) { alert("Can't remove scene with name: " + sceneName + ". There is no scene with that name."); return; }
+	this.scenes.splice(sp, 1);
+}
+
+SceneManager.prototype.setScene = function(sceneName)
+{
+	if(this.currentScene != null)
+	{
+		this.getPixiApplication().stage.removeChild(this.currentScene);
+		this.currentScene.onDestroy();
+	}
+	
+	this.currentScene = new this.scenes[this.getSceneIndex(sceneName)].scenePrototype;
+	this.getPixiApplication().stage.addChild(this.currentScene);
+	this.currentScene.onCreate();
+}
+
+SceneManager.prototype.getSceneIndex = function(sceneName)
+{
+	for(var i = 0; i < this.scenes.length; i++)
+	{
+		if(this.scenes[i].name == sceneName)
+		{
+			return i;
+		}
+	}
+
+	return -1;
+}
+
+SceneManager.prototype.update = function(deltaTime)
+{
+	if(this.currentScene == null) { return; }
+	this.currentScene.onUpdate(deltaTime);
+}
+
+function SceneManagerItem(name, prototype)
+{
+	this.name = name;
+	this.scenePrototype = prototype;
+}
+
+
+Scene.prototype = Object.create(PIXI.DisplayObjectContainer.prototype);
+function Scene()
+{
+	PIXI.DisplayObjectContainer.call(this);
+}
+
+Scene.prototype.onCreate = function()
+{
+
+}
+
+Scene.prototype.onUpdate = function(deltaTime)
+{
+
+}
+
+Scene.prototype.onDestroy = function()
+{
+
+}
